a1bc342f473f8987afdca8b7bac4c6bb20ce366a
SangHunKim <kim199603330@gmail.com>
2025-10-27 10:02:56 +0900

    monday 10am

--- src/main/java/controller/BoardListController.java
+++ src/main/java/controller/BoardListController.java
@@ -1,41 +1,75 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Vector;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BoardListController
- */
-@WebServlet("/BoardListController.do")
+import model.PostDAO;
+import model.PostDTO;
+
 public class BoardListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardListController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		reqPro(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		reqPro(request, response);
+	}
+
+	protected void reqPro(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 총 게시글 수 저장
+		int count = 0;
+
+		PostDAO bdao = new PostDAO();
+		count = bdao.getAllCount();
+
+		// 수정, 삭제시 받아오는 메시지
+		String msg = (String) request.getAttribute("msg");
+
+		// 한페이지에 보여줄 게시글 수
+		int pageSize = 10;
+
+		String pageNum = request.getParameter("pageNum");
+		if (pageNum == null) {
+			pageNum = "1";
+		}
+
+		int number = 0;
+		int currentPage = Integer.parseInt(pageNum);
+
+		// 페이지 시작번호, 페이지 끝번호
+		// currentPage 1 -> 1~10번 게시글
+		// currentPage 2 -> 11~20번 게시글
+		int startRow = (currentPage - 1) * pageSize + 1;
+		int endRow = currentPage * pageSize;
+
+		// 11개의 글일 (2-1)*10 = 1
+		number = count - (currentPage - 1) * pageSize;
+
+		Vector<PostDTO> v = bdao.getAllBoard(startRow, endRow);
+
+		request.setAttribute("count", count);
+		request.setAttribute("msg", msg);
+		request.setAttribute("v", v);
+		request.setAttribute("pageSize", pageSize);
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("number", number);
+
+		RequestDispatcher dis = request.getRequestDispatcher("boardList.jsp");
+		dis.forward(request, response);
+
 	}
 
 }
--- src/main/java/controller/BoardWriteProcController.java
+++ src/main/java/controller/BoardWriteProcController.java
@@ -1,41 +1,46 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BoardWriteProcController
- */
+import model.PostDAO;
+import model.PostDTO;
+
 @WebServlet("/BoardWriteProcController")
 public class BoardWriteProcController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardWriteProcController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		rePro(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		rePro(request, response);
 	}
+
+	protected void rePro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		PostDTO bean = new PostDTO();
+		bean.setWriter(request.getParameter("writer"));
+		bean.setSubject(request.getParameter("subject"));
+		bean.setEmail(request.getParameter("email"));
+		bean.setPassword(request.getParameter("password"));
+		bean.setContent(request.getParameter("content"));
+		
+		PostDAO bdao = new PostDAO();
+		bdao.insertBoard(bean);
+		
+		RequestDispatcher dis = request.getRequestDispatcher("BoardListController.do");
+		dis.forward(request, response);
+	}
+
 
 }
